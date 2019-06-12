872afca851fd1ff9f5ab5f9958b0f005e4d2e19b
Matthias Maderer <edvler@edvler-blog.de>
2019-06-12 17:27:58 +0200

    Added archive size check

--- check_mk/web/plugins/wato/check_parameters_proxmox_qemu_backup.py
+++ check_mk/web/plugins/wato/check_parameters_proxmox_qemu_backup.py
@@ -3,6 +3,7 @@
 # URL: https://github.com/edvler/check_mk_proxmox-qemu-backup
 # License: GPLv2
 
+
 register_check_parameters(
     subgroup_os,
     "proxmox",
@@ -24,21 +25,27 @@ register_check_parameters(
                  elements = [
                      Age(title=_("Warning at or above backupage"),
                          default_value = 93600, 
-                         display = [ "minutes" ],
                          help=_("If the backup is older than the specified time, the check changes to warning. (24h=1440m; 26h=1560m)")
                      ),
                      Age(title=_("Critical at or above backupage"),
                          default_value = 108000,
-                         display = [ "minutes" ],
                          help=_("If the backup is older than the specified time, the check changes to critical. (24h=1440m; 26h=1560m)")
                      ),
                  ]
              )
             ),
+            ("backup_minsize",
+                Tuple(
+                    title = _("Minimal size of vzdump archive"),
+                    elements = [
+                      Filesize(title = _("Warning if below")),
+                      Filesize(title = _("Critical if below")),
+                    ]
+                )
+            ),
             ('running_time',
                 Age(title=_("Runtime (default 30min) of the backup process"),
                     default_value = 1800,
-                    display = [ "minutes" ],
                     help=_("Define here how long the backup process is running. If the Backup is running, the check dont change to warning or error for the specified time. If the backup runs longer as specified here, the check changes to warning.")
                 ),
             )
